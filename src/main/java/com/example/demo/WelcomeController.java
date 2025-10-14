f7ef5c1e273e95203754073adbddbcb12e5d2aeb
Escape9002 <antonio.rehwinkel@gmail.com>
2025-10-14 23:26:55 +0000

    init

---
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    
+    @RequestMapping("/welcome")
+    String welcome() {
+        return "Hello there";
+    }
+
+    @RequestMapping("/")
+    String index() {
+        return "<html> <h1> This is the index of this wonderful thing </h1> <!html>";
+    }
+}
